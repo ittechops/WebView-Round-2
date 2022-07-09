92ea24046575dafcc718caf1c94ba4d1fe62f76b
Christopher H Day <cday1010@live.com>
2022-07-09 14:56:51 -0400

    Changes after removing the refernece to the TestLibrary.DLL class library and leveraging it from the Windows Forms application as a loaded runtime assembly.

--- Launcher/Form1.cs
+++ Launcher/Form1.cs
@@ -1,35 +1,47 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using TestLibrary;
 
 namespace Launcher
 {
     public partial class Form1 : Form
     {
-        Operations ops = new Operations();
+        string assemblyPath = string.Empty;
+        object ops = null;
+        Assembly assembly = null;
+        Type type = null;
+
         public Form1()
         {
             InitializeComponent();
+            //assemblyPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\TestLibrary.dll";
+            //assembly = Assembly.LoadFrom(assemblyPath);
+            assembly = Assembly.LoadFrom("C:\\Users\\christopherday\\source\\repos\\WebView Round 2\\TestLibrary\\bin\\Debug\\TestLibrary.dll");
+            type = assembly.GetType("TestLibrary.Operations");
+            ops = Activator.CreateInstance(type);
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            ops.LaunchUI();
-            
+            MethodInfo myMethod = type.GetMethod("LaunchUI");
+            myMethod.Invoke(ops, null);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            ops.ChangeUrl("https://microsoft.com");
-
+            MethodInfo myMethod = type.GetMethod("ChangeUrl");
+            myMethod.Invoke(ops, new object[] {"https://microsoft.com"});
+            //ops.ChangeUrl("https://microsoft.com");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -39,7 +51,9 @@ namespace Launcher
 
         private void button3_Click(object sender, EventArgs e)
         {
-            ops.ChangeUrl("https://amazon.com");
+            MethodInfo myMethod = type.GetMethod("ChangeUrl");
+            myMethod.Invoke(ops, new object[] { "https://amazon.com" });
+            //ops.ChangeUrl("https://amazon.com");
         }
     }
 }
